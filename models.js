ae4e665a71745e509e148e6e3eddeee41de722c4
Garrett Green <garrett.wile.green@gmail.com>
2018-09-24 16:55:40 -0500

    added veggie row to db

--- models.js
+++ models.js
@@ -24,6 +24,7 @@ Gardener.hasOne(Plot)
 Vegetable.belongsToMany(Plot, {through: 'vegetable_plot'})
 Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'})
 Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'})
-module.exports = db;
+
+module.exports = {db, Vegetable, Plot, Gardener}
 
 
--- seed.js
+++ seed.js
@@ -1,9 +1,19 @@
-const db = require('./models');
+const {db, Vegetable, Plot, Gardener} = require('./models');
+
+
 
 db.sync({force: true})
+
   .then(() => {
-    console.log('Database synced!');
+    const veggiePromise = Vegetable.create({name: 'carrots', color: 'orange', planted_on: `${new Date()}`})
+
+    const gardenerPromise = veggiePromise.then(vegetable => {
+      return Gardener.create({name: 'dr green thumb', age: 33, vegetableId: vegetable.id})
+    })
+
+    .then(() => {
       db.close();
+    })
   })
   .catch(err => {
     console.log('Distaster! Something went wrong!');
